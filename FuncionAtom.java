5b1d8f1e6af38dcb2632578d986a643f4ea314e6
belennmm <belenmmonterroso@gmail.com>
2024-03-11 09:15:13 -0600

    FuncionAtom implementación

--- FuncionAtom.java
+++ FuncionAtom.java
@@ -1,10 +1,61 @@
 public class FuncionAtom implements ReservedFunciones {
-    /*
-     * Clase que representa la función atom de lenguaje.
-     */
+
     @Override
     public void execute() {
-        // la lógica
+        if (Controlador.linea == null) {
+            System.out.println("La línea leída está vacía");
+        } 
+        else {
+            String argumento = obtenerArgumento(Controlador.linea);
+            String esAtomo = esAtomo(argumento);
+            System.out.println(esAtomo);
+        }
     }
 
+    
+    private String obtenerArgumento(String linea) {
+        // se busca el paréntisis de apertura
+        int inicio = linea.indexOf('(');
+        if (inicio != -1) {
+            // encontrar la posición del 1er espacio después del (
+            int espacio = linea.indexOf(' ', inicio);
+            if (espacio != -1) {
+                // encontrar la posición del 1er )  después del 1er espacio
+                int fin = linea.indexOf(')', espacio);
+                if (fin != -1) {
+                    // return la subcadena entre el primer espacio y el primer )
+                    return linea.substring(espacio + 1, fin);
+                }
+            }
+        }
+        // si no se encuentra return una cadena vacía
+        return "";
+    }
+    
+    
+    
+
+    
+    private String esAtomo(String cadena) {
+
+        // se muestra error si está sin argumento
+        if (cadena.trim().isEmpty()){
+            throw new IllegalArgumentException("El argumento no puede estar vacío");
+        }
+
+        // ver si es un número
+        try {
+            Double.parseDouble(cadena);
+            // true si sí es átomo
+            return "t"; 
+        }catch (NumberFormatException e) {
+            // falso/nil si no es átomo
+            if (cadena.isEmpty() || cadena.contains(" ")) {
+                return "nil";
+            } else {
+                return "t";
+            }
+        }
+    }
+    
 }
